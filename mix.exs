394da85565d2395e27b9b346736791ffdfa61902
George Cosma <georgemagic10@gmail.com>
2022-05-05 23:49:38 +0300

    Bumped version to v1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebFTP.MixProject do
   def project do
     [
       app: :web_ftp,
-      version: "0.1.0",
+      version: "1.0.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
